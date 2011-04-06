ae4be66c288ac58b3dd6ed28b09ab14ce03e9940
DanielSeifert <ds@shopmodule.com>
2011-04-06 14:06:28 +0000

    change net price tracking code

--- setup+doku/update.sql
+++ setup+doku/update.sql
@@ -3,3 +3,6 @@ UPDATE `d3_cfg_mod` SET `OXVERSION` = '2.1.0', `OXINSTALLDATE` = NOW(), `OXREVIS
 
 # ==== 2.1.0 PE4 => 2.1.1 PE4 / EE4 / CE4 ====
 UPDATE `d3_cfg_mod` SET `OXVERSION` = '2.1.1', `OXINSTALLDATE` = NOW(), `OXREVISION` = '16', `OXNEWREVISION` = '16' WHERE `OXMODID` = 'd3_googleanalytics';
+
+# ==== 2.1.1 PE4 => 2.1.2 PE4 / EE4 / CE4 ====
+UPDATE `d3_cfg_mod` SET `OXVERSION` = '2.1.2', `OXINSTALLDATE` = NOW(), `OXREVISION` = '18', `OXNEWREVISION` = '18' WHERE `OXMODID` = 'd3_googleanalytics';
