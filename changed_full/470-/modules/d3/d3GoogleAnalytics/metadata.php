70b5574c32377adc16517a13df9af440eafad5d5
DanielSeifert <ds@shopmodule.com>
2012-12-21 10:18:49 +0000

    fix metadata

--- changed_full/470-/modules/d3/d3GoogleAnalytics/metadata.php
+++ changed_full/470-/modules/d3/d3GoogleAnalytics/metadata.php
@@ -11,10 +11,10 @@ $aModule = array(
     'id'           => 'd3_googleanalytics',
     'title'        => (class_exists('d3utils')?d3utils::getInstance()->getD3Logo():'D&sup3;').' Google Analytics Schnittstelle',
     'description'  => array(
-            'de'   => 'Dieses Modul stellt Ihnen die schnelle und unkomplizierte Einbindung Ihres Google-Analytics-Kontos in Ihren Shop zur Verfügung.'
-                     .'Hierbei werden über standardisierte Schnittstellen die Besucherdaten und eCommerce-Daten zu Google übertragen. '
-                     .'Ebenfalls übermittelt werden Daten der Website-Suche. '
-                     .'Dem Modul liegen angepaßte Templates bei, mit denen auch die Trichter ordentlich protokolliert werden.',
+            'de'   => 'Dieses Modul stellt Ihnen die schnelle und unkomplizierte Einbindung Ihres Google-Analytics-Kontos in Ihren Shop zur Verf&uuml;gung.
+                       Hierbei werden &uuml;ber standardisierte Schnittstellen die Besucherdaten und eCommerce-Daten zu Google &uuml;bertragen.
+                       Ebenfalls &uuml;bermittelt werden Daten der Website-Suche.
+                       Dem Modul liegen angepa&szlig;te Templates bei, mit denen auch die Trichter ordentlich protokolliert werden.',
             'en'   => 'Provides a quick and easy integration with your Google Analytics account to your shop.',
     ),
     'thumbnail'    => 'picture.png',
@@ -23,20 +23,20 @@ $aModule = array(
     'email'        => 'support@shopmodule.com',
     'url'          => 'http://www.oxidmodule.com/',
     'extend'      => array(
-        'oxcmp_utils' => 'd3/d3_googleanalytics/modules/components/d3_oxcmp_utils_googleanalytics',
-        'oxorder'     => 'd3/d3_googleanalytics/modules/models/d3_oxorder_googleanalytics',
+        'oxcmp_utils' => 'd3/d3GoogleAnalytics/modules/components/d3_oxcmp_utils_googleanalytics',
+        'oxorder'     => 'd3/d3GoogleAnalytics/modules/models/d3_oxorder_googleanalytics',
     ),
     'files' => array(
-        'd3_cfg_googleanalytics'           => 'd3/d3_googleanalytics/controllers/admin/d3_cfg_googleanalytics.php',
-        'd3_cfg_googleanalytics_campaigns' => 'd3/d3_googleanalytics/controllers/admin/d3_cfg_googleanalytics_campaigns.php',
-        'd3_cfg_googleanalytics_licence'   => 'd3/d3_googleanalytics/controllers/admin/d3_cfg_googleanalytics_licence.php',
-        'd3_cfg_googleanalytics_list'      => 'd3/d3_googleanalytics/controllers/admin/d3_cfg_googleanalytics_list.php',
-        'd3_cfg_googleanalytics_main'      => 'd3/d3_googleanalytics/controllers/admin/d3_cfg_googleanalytics_main.php',
+        'd3_cfg_googleanalytics'           => 'd3/d3GoogleAnalytics/controllers/admin/d3_cfg_googleanalytics.php',
+        'd3_cfg_googleanalytics_campaigns' => 'd3/d3GoogleAnalytics/controllers/admin/d3_cfg_googleanalytics_campaigns.php',
+        'd3_cfg_googleanalytics_licence'   => 'd3/d3GoogleAnalytics/controllers/admin/d3_cfg_googleanalytics_licence.php',
+        'd3_cfg_googleanalytics_list'      => 'd3/d3GoogleAnalytics/controllers/admin/d3_cfg_googleanalytics_list.php',
+        'd3_cfg_googleanalytics_main'      => 'd3/d3GoogleAnalytics/controllers/admin/d3_cfg_googleanalytics_main.php',
     ),
     'templates' => array(
-        'd3_googleanalytics.tpl'         => 'd3/d3_googleanalytics/views/tpl/widget/d3_googleanalytics.tpl',
-        'd3_cfg_googleanalytics_main.tpl'         => 'd3/d3_googleanalytics/views/admin/tpl/d3_cfg_googleanalytics_main.tpl',
-        'd3_cfg_googleanalytics_campaigns.tpl'         => 'd3/d3_googleanalytics/views/admin/tpl/d3_cfg_googleanalytics_campaigns.tpl',
+        'd3_googleanalytics.tpl'                => 'd3/d3GoogleAnalytics/views/tpl/widget/d3_googleanalytics.tpl',
+        'd3_cfg_googleanalytics_main.tpl'       => 'd3/d3GoogleAnalytics/views/admin/tpl/d3_cfg_googleanalytics_main.tpl',
+        'd3_cfg_googleanalytics_campaigns.tpl'  => 'd3/d3GoogleAnalytics/views/admin/tpl/d3_cfg_googleanalytics_campaigns.tpl',
     ),
     'events'       => array(
         'onActivate'                      => 'd3install::checkUpdateStart',
