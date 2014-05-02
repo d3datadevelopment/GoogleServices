68c2b56af2b9d13d36907b9a95f616eb46dfd76a
RayEhrhardt <re@shopmodule.com>
2014-05-02 08:47:38 +0000

    Remoced .bak files and the "Kundenversion"

--- Sources/d3GoogleAnalytic_Kundenversion indivParams Thankyou/copy_this/modules/d3GoogleAnalytics/views/d3_thankyou_googleanalytics.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class d3_thankyou_googleAnalytics extends d3_thankyou_googleAnalytics_parent {
-	public function isNewCustomer() {
-		$isNew = oxSession::getVar("newcustomer");
-		oxSession::deleteVar("newcustomer");
-		return $isNew;
-	}
-}
