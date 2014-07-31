516773a872f1e3b69d7c84bb0b46c86b76df7ec9
DanielSeifert <ds@shopmodule.com>
2014-07-31 12:14:30 +0000

    revision adjusted

--- copy_this/modules/d3/d3_googleanalytics/metadata.php
+++ copy_this/modules/d3/d3_googleanalytics/metadata.php
@@ -38,7 +38,7 @@ $aModule = array(
             'en'   => 'Provides a quick and easy integration with your Google Analytics account to your shop.',
     ),
     'thumbnail'    => 'picture.png',
-    'version'      => '3.2.0.0',
+    'version'      => '3.2.1.0',
     'author'       => 'D&sup3; Data Development (Inh. Thomas Dartsch)',
     'email'        => 'support@shopmodule.com',
     'url'          => 'http://www.oxidmodule.com/',
